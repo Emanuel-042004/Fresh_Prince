7bfba1c83f74c02dd30291180fe8a3db38f9493a
Anyecarqui <nanyacq02@gmail.com>
2024-09-13 17:05:23 -0500

    showcard modificada ejemplo

--- modules/showCard.js
+++ modules/showCard.js
@@ -1,30 +1,19 @@
 export const showCard = (fyvContainer, dataFrutasyVerduras) => {
     dataFrutasyVerduras?.forEach((element) => {
-      const { id, name, image, description, categorie, price, discount } = element;
+      const { id, name, price, description, collection, img1, img2, img3 } = element;
       console.log(fyvContainer)
       fyvContainer.innerHTML += `    
          <div class="card text-center my-3" >
-              <img id=${id} data-bs-toggle="modal" data-bs-target="#exampleModal${id}" src=${image} class="card-img-top d-block m-auto w-15" />
+              <img id=${id} data-bs-toggle="modal" data-bs-target="#exampleModal${id}" src=${img1} class="card-img-top d-block m-auto w-15" />
               <div class="card-body">
-              <h4><del>${discount}</del></h4>
                 <h5 class="card-title">${name}</h5>
                <h6>${price}</h6>
-                    <h6>${categorie}</h6>
+                    <p>${description}</p>
                 <button type="button"  class="btn d-block btn-success" id=${id} data-bs-toggle="modal" data-bs-target="#exampleModal${id}"
                   ><i class="bi bi-star text-light"> Detalle </i></button>
                   
               </div>
             </div>     
-         
-      
-         <div class="modal fade" id="exampleModal${id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">${name}</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                 
-                </div>
                 <div class="modal-body">
                <img src=${image} class="card-img-top d-block m-auto w-25" alt="..." />
                 <u>${discount}</u> 
