3c52f98a0ab66682c617b716ea1f9a97cebb15f3
Anyecarqui <nanyacq02@gmail.com>
2024-09-13 17:33:46 -0500

    modificaciones

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
 import { postData } from "../modules/postData.js";
-import { searchData } from "../modules/searchData.js";
 import { showCard } from "../modules/showCard.js";
-import { frutasyverduras } from "../scripts/constant.js";
+import { clothes } from "../scripts/constant.js";
+
+import { getData } from "../modules/getData.js";
+
+const clothesContainer = document.getElementById('displayclothes');
+
+document.addEventListener("DOMContentLoaded", async ()=> {
+  
+  const dataclothes = await getData('http://localhost:3000/clothes');
+
+  showCard(clothesContainer, dataclothes);
+})
